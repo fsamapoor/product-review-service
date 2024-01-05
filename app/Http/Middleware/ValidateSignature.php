b37804b307a95b7d56c5d6084b148e0f72f8e7d8
Faraz Samapoor <f.samapoor@gmail.com>
2024-01-05 18:12:40 +0330

    Bumps type-coverage to %100.
    Signed-off-by: Faraz Samapoor <f.samapoor@gmail.com>

--- app/Http/Middleware/ValidateSignature.php
+++ app/Http/Middleware/ValidateSignature.php
@@ -13,7 +13,7 @@ class ValidateSignature extends Middleware
      *
      * @var array<int, string>
      */
-    protected $except = [
+    protected $except = [ // @pest-ignore-type
         // 'fbclid',
         // 'utm_campaign',
         // 'utm_content',
