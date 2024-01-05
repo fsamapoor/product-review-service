7cd3dd6ce0d369dd764e7c43a4d66d2f6387514a
Faraz Samapoor <f.samapoor@gmail.com>
2024-01-05 18:10:20 +0330

    Adds basic architectural test.
    Signed-off-by: Faraz Samapoor <f.samapoor@gmail.com>

---
+++ tests/Feature/ArchTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+test('the codebase does not reference env variables outside of config files')
+    ->expect('env')
+    ->not->toBeUsed();
+
+test('app')
+    ->expect('App')
+    ->toUseStrictTypes();
+
+test('the codebase does not contain any debugging code')
+    ->expect([
+        'dd',
+        'dump',
+        'var_dump',
+        'die',
+        'sleep',
+        'usleep',
+        'exit',
+    ])
+    ->not->toBeUsed();
