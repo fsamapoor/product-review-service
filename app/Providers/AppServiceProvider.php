53a6800f2abe4e482edfa25b3af64200325c92fe
Faraz Samapoor <f.samapoor@gmail.com>
2024-01-05 19:27:09 +0330

    Adds safety mechanisms.
    Signed-off-by: Faraz Samapoor <f.samapoor@gmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::unguard();
+        Model::preventLazyLoading(! $this->app->isProduction());
+        Model::preventAccessingMissingAttributes();
     }
 }
