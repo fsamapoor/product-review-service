a033ddc04c50340162aa596f016fe8a40a0eef31
Faraz Samapoor <f.samapoor@gmail.com>
2024-01-07 03:00:08 +0330

    Removes the default api route.
    Signed-off-by: Faraz Samapoor <f.samapoor@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ReviewController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,10 +16,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::apiResource('products', ProductController::class)
     ->only(['index']);
